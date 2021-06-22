257f1e27424560e17870c07ffc32ba33aeedc57b
vsavkin <vic.savkin@gmail.com>
2021-06-22 10:39:27 -0400

    add ci setup

--- apps/admin/src/app/app.component.spec.ts
+++ apps/admin/src/app/app.component.spec.ts
@@ -17,15 +17,6 @@ describe('AppComponent', () => {
   it(`should have as title 'admin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('admin');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to admin!'
-    );
+    expect(app.title).toEqual('ADMIN');
   });
 });
